7871b94dc7446b8e3933be85345787e0b2b4158a
DESKTOP-G2RUKHA\F309 <D1084181702@gm.lhu.edu.tw>
2020-10-08 15:18:52 +0800

    新增其他功能網頁

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,38 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* Gun */
+Route::get('guns/create', function(){
+    return view('guns.create');
+});
+
+Route::get('guns/{id}/edit', function($id){
+    return view('guns.edit');
+})->where('id', '[0-9]+');
+
+Route::get('guns', function(){
+    return view('guns.index');
+});
+
+Route::get('guns/{id}', function($id){
+    return view('guns.show');
+})->where('id', '[0-9]+');
+
+
+/* Type */
+Route::get('type/create', function(){
+    return view('type.create');
+});
+
+Route::get('type/{id}/edit', function($id){
+    return view('type.edit');
+})->where('id', '[0-9]+');
+
+Route::get('type', function(){
+    return view('type.index');
+});
+
+Route::get('type/{id}', function($id){
+    return view('type.show');
+})->where('id', '[0-9]+');
